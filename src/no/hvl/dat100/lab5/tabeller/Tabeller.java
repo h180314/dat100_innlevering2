fec7e48f1f29938e20af0abe76ec598911b35d80
h180314 <180314@stud.hvl.no>
2023-10-04 19:49:46 +0200

    innlevering2

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -6,7 +6,11 @@ public class Tabeller {
 	public static void skrivUt(int[] tabell) {
 
 		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+		//throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+		int[] tab = tabell;
+		for (int i = 0; i < tab.length; i++) {
+			System.out.println(tab[i]);
+		}
 
 	}
 
@@ -14,7 +18,18 @@ public class Tabeller {
 	public static String tilStreng(int[] tabell) {
 
 		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		//throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		int[] tab = tabell;
+		String tabS = "[";
+		for (int i = 0; i < tab.length; i++) {
+			tabS += tabell[i];
+			if (i < tab.length-1 )
+				tabS += ",";
+		}
+		tabS += "]";
+		//System.out.println(tabS);
+		return
+				tabS;
 		
 	}
 
@@ -22,29 +37,98 @@ public class Tabeller {
 	public static int summer(int[] tabell) {
 
 		// TODO
-		throw new UnsupportedOperationException("Metoden summer ikke implementert");
+		//throw new UnsupportedOperationException("Metoden summer ikke implementert");
+		
+		//vanlig for-løkke
+		int[] tab = tabell;
+		int sum = 0;
+//		for (int i = 0; i < tab.length; i++) {
+//			sum += tab[i];
+//		}
+		
+		//while løkke
+//		int i = 0; sum = 0;
+//		if (tab.length > 0) {
+//			do {
+//				sum += tab[i];
+//				i++;
+//			} while (i < tab.length);
+//		}
+
+		
+//		//utvidet for-løkke
+		sum = 0; 
+		for (int tabI: tab)
+			sum += tabI;
+		
+		return
+				sum;
 	}
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
 
 		// TODO
-		throw new UnsupportedOperationException("Metoden finnesTall ikke implementert");
+		//throw new UnsupportedOperationException("Metoden finnesTall ikke implementert");
+		int match = tall;
+		int[] tab = tabell;
+		int i = 0;
+		boolean finnes;
+		finnes = false;
+		if (tab.length > 0) {
+			while (i < tab.length) {
+				if (tab[i] == match) {
+					finnes = true; break;
+				}
+				else
+					i++;
+			}
+		}
+		return
+				finnes;
+		
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
 		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
-
+		//throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
+		int[] tab = tabell;
+		int index = -1;
+		int i = 0;
+		int tallet = tall;
+		boolean finnes;
+		if (tab.length > 0) {
+			while (i < tab.length) {
+				if (tab[i] == tallet) {
+					index = i;
+					finnes = true; break;
+				}
+				else
+					i++;
+			}
+		}
+			return
+					index;
 	}
 
 	// f)
 	public static int[] reverser(int[] tabell) {
 
 		// TODO
-		throw new UnsupportedOperationException("Metoden reverser ikke implementert");
+		//throw new UnsupportedOperationException("Metoden reverser ikke implementert");
+		int[] tab = tabell;
+		int[] tab_rev;
+		tab_rev =  new int[tab.length];
+		if (tab.length > 0) {
+			for (int i = 0; i < tab.length; i++) {
+				tab_rev[i] = tab[tab.length - 1 - i];
+				//System.out.println(tab_rev[i]);
+			}
+		}
+		return
+				tab_rev;
 		
 	}
 
@@ -52,13 +136,46 @@ public class Tabeller {
 	public static boolean erSortert(int[] tabell) {
 
 		// TODO
-		throw new UnsupportedOperationException("Metoden erSortert ikke implementert");
+		//throw new UnsupportedOperationException("Metoden erSortert ikke implementert");
+		int[] tab = tabell;
+		boolean sort = true;
+		if (tab.length > 0) {
+			for (int i = 1; i < tab.length; i++) {
+				if (tab[i] < tab[i-1]) {
+					sort = false;
+				}
+			}
+		}
+		return
+				sort;
 	}
 
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
 
 		// TODO
-		throw new UnsupportedOperationException("Metoden settSammen ikke implementert");
+		//throw new UnsupportedOperationException("Metoden settSammen ikke implementert");
+		int[] tab1 = tabell1;
+		int[] tab2 = tabell2;
+		int[] tab_sammen;
+		tab_sammen =  new int[tab1.length + tab2.length];
+		int k = 0;
+		if (tab1.length > 0) {
+			for (int i = 0; i < tab1.length; i++) {
+				tab_sammen[i] = tab1[i];
+				k++;
+				//System.out.println(tab_sammen[i]);
+			}
+		}
+		if (tab2.length > 0) {
+			for (int j = 0; j < tab2.length; j++) {
+				tab_sammen[k+j] = tab2[j];
+				//System.out.println(tab_sammen[k+j]);
+			}
+		}
+			
+			
+		return
+				tab_sammen;
 	}
 }
--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -6,14 +6,34 @@ public class Matriser {
 	public static void skrivUt(int[][] matrise) {
 		
 		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+		//throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+		
+		for (int[] matI: matrise) {   
+			System.out.println();
+			for (int matII: matI)
+				System.out.print(" " + matII);
+		}
+		System.out.println();
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
 		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		//throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		String matS = "";
+		for (int[] matI: matrise) {   
+			for (int matII: matI) {
+				matS += matII;
+				matS += " ";
+
+			}
+			matS += "\n";
+		}
+		
+		
+		return
+				matS;
 		
 	}
 
@@ -21,7 +41,22 @@ public class Matriser {
 	public static int[][] skaler(int tall, int[][] matrise) {
 		
 		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
+		//throw new UnsupportedOperationException("Metoden skaler ikke implementert");
+		int[][] ny_mat;
+		int[] tab_temp;
+		ny_mat = new int[matrise.length][];
+		for (int i = 0; i < matrise.length; i++) {
+			tab_temp = new int[matrise[i].length];
+			for(int j = 0; j < matrise[i].length; j++) {
+			    //System.out.println(matrise[i][j]);
+				tab_temp[j] = matrise[i][j] * tall;
+				System.out.println(tab_temp[i]);
+			}
+			ny_mat[i] = tab_temp;
+		}
+		return
+				ny_mat;
+
 	
 	}
 
